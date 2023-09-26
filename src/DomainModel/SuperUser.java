efa95d03454f5061893f10a5b90397c0de5723a0
digesaremarco <digesaremarco@gmail.com>
2023-09-26 17:00:57 +0200

    created class "SuperUser" using Singleton

--- src/DomainModel/SuperUser.java
+++ src/DomainModel/SuperUser.java
@@ -1,4 +1,16 @@
 package DomainModel;
 
-public class SuperUser {
+public class SuperUser extends WebUser {
+
+    private static SuperUser istance = null;
+
+    private SuperUser(int c, String u, String p) {
+        super(c, u, p);
+    }
+
+    public static SuperUser getIstance(int c, String u, String p) {
+        if (istance == null)
+            istance = new SuperUser(c, u, p);
+        return istance;
+    }
 }
--- src/DomainModel/WebUser.java
+++ src/DomainModel/WebUser.java
@@ -3,11 +3,15 @@ package DomainModel;
 public class WebUser {
     private int coduser;
     private boolean logged;
+    private String username;
+    private String password;
 
 
-    public WebUser(int c) {
+    public WebUser(int c, String u, String p) {
         coduser = c;
         logged = false;
+        username = u;
+        password = p;
     }
 
     public boolean getLogged() {
