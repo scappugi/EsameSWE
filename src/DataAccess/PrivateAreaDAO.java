5f7fb32b76f6880e16098e9a51b2c1ea90cbdcdf
scappugi <simone.cappugi@stud.unifi.it>
2023-09-26 17:50:55 +0200

    create some method for DAO

--- src/DataAccess/PrivateAreaDAO.java
+++ src/DataAccess/PrivateAreaDAO.java
@@ -1,5 +1,24 @@
 package DataAccess;
 
+import DomainModel.Order;
+
+import java.util.ArrayList;
+
 public class PrivateAreaDAO {
+
+    public PrivateAreaDAO(){}
+
+    //manca il modifyDaty() non so a cosa serve
+    public ArrayList<Order> getAllOrder(){
+        ArrayList<Order> orders = null;
+        return orders;
+    }
+    public Order getOrder(Order o){
+        return o;
+    }
+
+    public boolean addOrder(){
+        return true;
+    }
 
 }
--- src/DataAccess/SuperUserDAO.java
+++ src/DataAccess/SuperUserDAO.java
@@ -1,4 +1,20 @@
 package DataAccess;
 
+import BuisnessLogic.Clothes;
+
 public class SuperUserDAO {
+    public SuperUserDAO(){}
+    public boolean addNewClothes(Clothes clothes){
+        return true;
+    }
+
+    public boolean removeClothes(Clothes clothes){
+        return true;
+    }
+
+    public boolean updateClothes(Clothes clothes){
+        return true;
+    }
+
+
 }
