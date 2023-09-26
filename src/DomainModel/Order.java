aa2f59dca075550605ecf827083bff487f3f80fa
digesaremarco <digesaremarco@gmail.com>
2023-09-26 15:50:47 +0200

    created class "Order"

--- src/DomainModel/Order.java
+++ src/DomainModel/Order.java
@@ -1,5 +1,24 @@
 package DomainModel;
 
+import java.util.Date;
+
 public class Order {
     private String id;
+    private Date ordered; //when the order was placed
+    private Date shipped; //when the order was shipped
+    private String shipto;
+    private String status;
+
+    public Order(String id, Date o, String sh, String s) {
+        this.id = id;
+        ordered = o;
+        shipped = null;
+        shipto = sh;
+        status = s;
+    }
+
+    public void setShipped(Date date){
+        shipped = date;
+    }
+
 }
