e24ab24e16bac87134b6b096bee246e426171ce4
digesaremarco <digesaremarco@gmail.com>
2023-09-26 15:27:56 +0200

    created class "WebUser"

--- src/DomainModel/WebUser.java
+++ src/DomainModel/WebUser.java
@@ -1,4 +1,19 @@
 package DomainModel;
 
 public class WebUser {
+    private int coduser;
+    private boolean logged;
+
+    public boolean getLogged() {
+        return logged;
+    }
+
+    public void setLogged(boolean l) {
+        logged = l;
+    }
+
+    public int getCod(){
+        return coduser;
+    }
+
 }
