3128cab5fa0b6d172b683c6c9b05559a263f3331
digesaremarco <digesaremarco@gmail.com>
2023-09-27 11:37:57 +0200

    created class "DecoratorSearchBrand", it's used to filter the clothes by brand

---
+++ src/DomainModel/Search/DecoratorSearchBrand.java
@@ -0,0 +1,32 @@
+package DomainModel.Search;
+
+import DomainModel.Clothes;
+
+import java.util.ArrayList;
+
+public class DecoratorSearchBrand extends BaseDecoratorSearch{
+    private String brand;
+
+    public DecoratorSearchBrand(Search search, String brand) {
+        super(search);
+        this.brand = brand;
+    }
+
+
+    @Override
+    public ArrayList<Clothes> searchClothes() {
+        ArrayList<Clothes> basequery = null;
+        ArrayList<Clothes> filtered = new ArrayList<>();
+        try {
+            basequery = search.searchClothes();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        for(Clothes cl : basequery){
+            if(cl.getBrand() == brand)
+                filtered.add(cl);
+        }
+        return filtered;
+    }
+}
