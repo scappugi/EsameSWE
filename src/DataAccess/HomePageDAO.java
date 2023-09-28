bafcd35accd96fe01f9979104ff2a3e7b22c8aae
digesaremarco <digesaremarco@gmail.com>
2023-09-28 10:40:00 +0200

    created login method

--- src/DataAccess/HomePageDAO.java
+++ src/DataAccess/HomePageDAO.java
@@ -6,15 +6,44 @@ import DomainModel.Sweatshirt;
 import DomainModel.Trousers;
 import DomainModel.WebUser;
 
+import java.sql.*;
 import java.util.ArrayList;
 
 public class HomePageDAO {
+
+    private Connection connection;
+
+    public HomePageDAO(String databaseURL) { //C:/sqlite/ShopOnline.db
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:" + databaseURL);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     public boolean registerUser(WebUser webUser){
         return true;
     }
 
-    public boolean login(String username, String password){return true;}
+    public boolean login(String username, String password){
+        String query = "SELECT * FROM WebUser WHERE username = ? AND password = ?";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, password);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            // If there is a line in the result, it means the credentials are correct
+            if (resultSet.next()) {
+                return true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        // If there is no match, return false
+        return false;
+    }
 
     public boolean registerCreditCard(/**/){
         return true;
