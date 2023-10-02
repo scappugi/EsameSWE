32705588a33e29e78181428913cdb04a9415f737
digesaremarco <digesaremarco@gmail.com>
2023-10-02 15:41:58 +0200

    created method to update clothes qty

--- src/DataAccess/HomePageDAO.java
+++ src/DataAccess/HomePageDAO.java
@@ -27,8 +27,8 @@ public class HomePageDAO {
         }
     }
 
-    public boolean registerUser(String username, String password)throws SQLException{
-        if (!login(username,password)) {
+    public boolean registerUser(String username, String password) throws SQLException {
+        if (!login(username, password)) {
             String query = "INSERT INTO WebUser (UserName, Password) VALUES (?, ?)";
             PreparedStatement preparedStatement = connection.prepareStatement(query);
             preparedStatement.setString(1, username);
@@ -49,7 +49,7 @@ public class HomePageDAO {
         return false; // the user is already registred
     }
 
-    public boolean login(String username, String password){
+    public boolean login(String username, String password) {
         String query = "SELECT * FROM WebUser WHERE username = ? AND password = ?";
 
         try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
@@ -66,42 +66,43 @@ public class HomePageDAO {
             e.printStackTrace();
         }
         // If there is no match, return false
-        return false;    }
+        return false;
+    }
 
-    public boolean registerCreditCard(DebitCard debitcard,String username) throws SQLException {
+    public boolean registerCreditCard(DebitCard debitcard, String username) throws SQLException {
         int codUser = 0;
         String selectQuery = "SELECT * FROM WebUser WHERE userName = ?"; //ricerca user
-            PreparedStatement preparedStatement = connection.prepareStatement(selectQuery);
-            preparedStatement.setString(1, username);
-            ResultSet resultSet = preparedStatement.executeQuery();
+        PreparedStatement preparedStatement = connection.prepareStatement(selectQuery);
+        preparedStatement.setString(1, username);
+        ResultSet resultSet = preparedStatement.executeQuery();
 
-            if (resultSet.next()) {
-                // Leggi i dati dell'utente dal ResultSet e crea un oggetto WebUser
-                codUser = resultSet.getInt("codUser"); //una volta trovato l' user inserisce la carta
-                String insertQuery = "INSERT INTO DebitCard (codCard, CVV, Date, UserID) VALUES (?, ?, ?, ?)";
-                PreparedStatement preparedStatement1 = connection.prepareStatement(insertQuery);
-                preparedStatement1.setInt(1, debitcard.getCodCard());
-                preparedStatement1.setInt(2, debitcard.getCVV());
-                preparedStatement1.setDate(3, (Date) debitcard.getDate());
-                preparedStatement1.setInt(4, codUser);
+        if (resultSet.next()) {
+            // Leggi i dati dell'utente dal ResultSet e crea un oggetto WebUser
+            codUser = resultSet.getInt("codUser"); //una volta trovato l' user inserisce la carta
+            String insertQuery = "INSERT INTO DebitCard (codCard, CVV, Date, UserID) VALUES (?, ?, ?, ?)";
+            PreparedStatement preparedStatement1 = connection.prepareStatement(insertQuery);
+            preparedStatement1.setInt(1, debitcard.getCodCard());
+            preparedStatement1.setInt(2, debitcard.getCVV());
+            preparedStatement1.setDate(3, (Date) debitcard.getDate());
+            preparedStatement1.setInt(4, codUser);
 
-                int rowsInserted = preparedStatement1.executeUpdate();
+            int rowsInserted = preparedStatement1.executeUpdate();
 
-                return rowsInserted > 0; //se aggiungo almeno una riga ritorno true
-            }
+            return rowsInserted > 0; //se aggiungo almeno una riga ritorno true
+        }
         return false;
     }
 
     //search if the qnt is not 0
-    public int checkAvailability(Clothes clothes){
-        int qty=0;
+    public int checkAvailability(Clothes clothes) {
+        int qty = 0;
 
         String selectQuery = "SELECT * FROM Clothes WHERE codClothes = ?";
         try (PreparedStatement preparedStatement = connection.prepareStatement(selectQuery)) {
             preparedStatement.setInt(1, clothes.getCodclothes());
 
             ResultSet resultSet = preparedStatement.executeQuery();
-                qty = resultSet.getInt("qty");
+            qty = resultSet.getInt("qty");
 
         } catch (SQLException e) {
             e.printStackTrace();
@@ -110,7 +111,7 @@ public class HomePageDAO {
     }
 
     public void updateAvailability(Clothes clothes, int newqty) {
-            String query = "UPDATE Clothes SET qty = ? WHERE codClothes = ?";
+        String query = "UPDATE Clothes SET qty = ? WHERE codClothes = ?";
         try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
             preparedStatement.setInt(1, newqty);
             preparedStatement.setInt(2, clothes.getCodclothes());
@@ -120,25 +121,24 @@ public class HomePageDAO {
         }
     }
 
-    public boolean checkDescription(Clothes clothes){
+    public boolean checkDescription(Clothes clothes) {
         return true;
     }
 
-    public ArrayList<Trousers> getTrousers(/*bisogna mettere la roba su cui filtrare(dal DB)*/){
+    public ArrayList<Trousers> getTrousers(/*bisogna mettere la roba su cui filtrare(dal DB)*/) {
         ArrayList<Trousers> trousers1 = null;
-        return trousers1 ; //dovrà ritornare qualcosa di sensato
+        return trousers1; //dovrà ritornare qualcosa di sensato
     }
 
-    public ArrayList<Sweatshirt> getSweatshirt(){
+    public ArrayList<Sweatshirt> getSweatshirt() {
         ArrayList<Sweatshirt> sweatshirts = null;
         return sweatshirts;
     }
 
-    public ArrayList<Shirt> getShirt(){
+    public ArrayList<Shirt> getShirt() {
         ArrayList<Shirt> shirts = null;
         return shirts;
     }
-
 
 
 }
