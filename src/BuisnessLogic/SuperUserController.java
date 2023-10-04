353985b817903853d4f4d8fe1c5b5fa1a0ddd2b7
digesaremarco <digesaremarco@gmail.com>
2023-10-04 10:59:48 +0200

    refactor name

--- src/BuisnessLogic/SuperUserController.java
+++ src/BuisnessLogic/SuperUserController.java
@@ -10,9 +10,10 @@ public class SuperUserController {
 
     SuperUser superuser;
     SuperUserDAO superUserDAO;
-    public SuperUserController (SuperUser su, SuperUserDAO suD){
+
+    public SuperUserController(SuperUser su, SuperUserDAO suD) {
         superuser = su;
-        superUserDAO=suD;
+        superUserDAO = suD;
         AbstractFactory factory1 = new ConcreteFactoryBrand1();
         // dao.aggiugi fabbrica
         AbstractFactory factory2 = new ConcreteFactoryBrand2();
@@ -23,22 +24,20 @@ public class SuperUserController {
 
     }
 
-    public boolean catalogaNuoviVestiti(String category, String size, String color, int qnt, AbstractFactory factory){
+    public boolean addNewClothes(String category, String size, String color, int qnt, AbstractFactory factory) {
         for (AbstractFactory it : superuser.getFactories()) {
             Clothes clothes;
-            if ( superuser.getFactories() == factory){ //check if the factory is right
-                if(category.equals("shirt")){ //look what category is clothes
-                    clothes = it.createShirt(size,color);
-                    superUserDAO.addNewClothes(clothes,qnt); //pass the qnt to db
+            if (superuser.getFactories() == factory) { //check if the factory is right
+                if (category.equals("shirt")) { //look what category is clothes
+                    clothes = it.createShirt(size, color);
+                    superUserDAO.addNewClothes(clothes, qnt); //pass the qnt to db
                     return true;
-                }
-                else if(category.equals("sweatshirt")){
-                    clothes = it.createSweatShirt(size,color);
+                } else if (category.equals("sweatshirt")) {
+                    clothes = it.createSweatShirt(size, color);
                     superUserDAO.addNewClothes(clothes, qnt);
                     return true;
-                }
-                else if(category.equals("trousers")){
-                    clothes = it.createTrousers(size,color);
+                } else if (category.equals("trousers")) {
+                    clothes = it.createTrousers(size, color);
                     superUserDAO.addNewClothes(clothes, qnt);
                     return true;
                 }
@@ -50,17 +49,13 @@ public class SuperUserController {
     }
 
 
-    public boolean modifyExistingClothes(Clothes oldclothes, Clothes newclothes){
+    public boolean modifyExistingClothes(Clothes oldclothes, Clothes newclothes) {
 
         return true;
     }
 
-    public boolean deleateExistingClothes(Clothes clothes){
+    public boolean deleteExistingClothes(Clothes clothes) {
 
-        return true;
-    }
-
-    public boolean buyNewClothes(){
         return true;
     }
 }
