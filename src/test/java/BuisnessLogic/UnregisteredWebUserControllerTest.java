22a63d950db47c5eab795d5a93e57dd211042b95
digesaremarco <digesaremarco@gmail.com>
2023-10-05 11:23:02 +0200

    update

---
+++ src/test/java/BuisnessLogic/UnregisteredWebUserControllerTest.java
@@ -0,0 +1,17 @@
+package BuisnessLogic;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UnregisteredWebUserControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void registerWebUser() {
+    }
+}
