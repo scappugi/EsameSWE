ca85df937b7c8284cd1f9606197396f4dca27428
digesaremarco <digesaremarco@gmail.com>
2023-10-05 11:10:03 +0200

    update

--- src/main/java/BuisnessLogic/UnregisteredWebUserController.java
+++ src/main/java/BuisnessLogic/UnregisteredWebUserController.java
@@ -2,17 +2,15 @@ package BuisnessLogic;
 
 import DataAccess.CartDAO;
 import DataAccess.HomePageDAO;
-import DataAccess.PrivateAreaDAO;
 import DomainModel.RegisteredWebUser;
-import DomainModel.WebUser;
 
 import java.sql.SQLException;
 
-public class UnregistredWebUserController {
+public class UnregisteredWebUserController {
 
     CartDAO cartDAO;
     HomePageDAO homepageDAO;
-    public UnregistredWebUserController(CartDAO cartDAO, HomePageDAO homepageDAO) {
+    public UnregisteredWebUserController(CartDAO cartDAO, HomePageDAO homepageDAO) {
         this.cartDAO = cartDAO;
         this.homepageDAO = homepageDAO;
 
