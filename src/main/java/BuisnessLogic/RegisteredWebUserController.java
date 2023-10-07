3fb704449fa45dd726076a773aee6aea709266cf
digesaremarco <digesaremarco@gmail.com>
2023-10-07 12:08:38 +0200

    Added method to  login, logout, add card, remove card and get all cards

--- src/main/java/BuisnessLogic/RegisteredWebUserController.java
+++ src/main/java/BuisnessLogic/RegisteredWebUserController.java
@@ -4,6 +4,7 @@ import DataAccess.CartDAO;
 import DataAccess.HomePageDAO;
 import DataAccess.PrivateAreaDAO;
 import DomainModel.Clothes;
+import DomainModel.DebitCard;
 import DomainModel.RegisteredWebUser;
 import DomainModel.Search.DecoratorSearchBrand;
 import DomainModel.Search.DecoratorSearchPrice;
@@ -11,6 +12,7 @@ import DomainModel.Search.DecoratorSearchSize;
 import DomainModel.Search.SearchConcrete;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class RegisteredWebUserController {
     private CartDAO cartDAO;
@@ -82,28 +84,55 @@ public class RegisteredWebUserController {
     public boolean buyCart() {
         boolean payed = false;
         if (!registeredwebuser.getCart().getMap().isEmpty()) {
-                cartDAO.payCartItem(registeredwebuser.getCart(), registeredwebuser.getUsername());
-                payed = true;
-                registeredwebuser.getCart().getMap().clear();
-            }
+            cartDAO.payCartItem(registeredwebuser.getCart(), registeredwebuser.getUsername());
+            payed = true;
+            registeredwebuser.getCart().getMap().clear();
+        }
         return payed;
     }
 
 
-    public ArrayList<Clothes> searchByPrice(int price){
-       DecoratorSearchPrice decoratorprice = new DecoratorSearchPrice(this.search, price);
+    public ArrayList<Clothes> searchByPrice(int price) {
+        DecoratorSearchPrice decoratorprice = new DecoratorSearchPrice(this.search, price);
         return decoratorprice.searchClothes();
     }
 
-    public ArrayList<Clothes> searchBySize(String size){
+    public ArrayList<Clothes> searchBySize(String size) {
         DecoratorSearchSize decoratorsize = new DecoratorSearchSize(this.search, size);
         return decoratorsize.searchClothes();
     }
 
-    public ArrayList<Clothes> searchByBrand(String brand){
+    public ArrayList<Clothes> searchByBrand(String brand) {
         DecoratorSearchBrand decoratorbrand = new DecoratorSearchBrand(this.search, brand);
         return decoratorbrand.searchClothes();
     }
 
-    //mancano i metodi per aggiungere e rimuovere le carte di credito + metodo per login e logout
+    public RegisteredWebUser login(String username, String password) {
+        RegisteredWebUser user = homepageDAO.login(username, password);
+        user.setLogged(true);
+        return user;
+    }
+
+    public void logout(RegisteredWebUser user) {
+        user.setLogged(false);
+    }
+
+    public boolean addCDebitCard(int code, int CVV, Date date) {
+        DebitCard card = new DebitCard(code, CVV, date);
+        if (registeredwebuser.getLogged())
+            return homepageDAO.registerCreditCard(card, registeredwebuser.getUsername());
+        else return false;
+    }
+
+    public boolean removeDebitCard(int code, int CVV) {
+        if (registeredwebuser.getLogged())
+            return homepageDAO.removeCreditCard(code, CVV, registeredwebuser.getUsername());
+        else return false;
+    }
+
+    public ArrayList<DebitCard> getAllCard() {
+        if (registeredwebuser.getLogged())
+            return homepageDAO.getAllDebitCards(registeredwebuser.getUsername());
+        else return null;
+    }
 }
